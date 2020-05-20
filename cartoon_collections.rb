0c8c9d4e1e781e069e9e41859bf666e5f6101212
Shadman Ahmed <shadman@outlook.com>
2020-05-20 22:51:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-planeteer_calls.any? do |calls|
+planeteer_calls.find do |calls|
   valid_calls.include? (calls)
   end
 end
